81d82870be0d1b44c0a9dc97e4d2e6087b13f88c
ranjan <ranjan.balappa@gmail.com>
2021-07-07 16:33:46 +0530

    Added setting seed and method to calculate performance metrics

--- cv/utils.py
+++ cv/utils.py
@@ -2,7 +2,11 @@
 # Utils to support training
 
 import torch
-from typing import Mapping, Dict, Any
+from typing import Mapping, Dict, Any, List
+import random
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+
 
 class Averagemeter:
     def __init__(self):
@@ -22,6 +26,40 @@ class Averagemeter:
         self.avg = self.sum / self.count 
         
         
-def prefix_all_keys(d: Mapping[str, Any], prefix: str) -> Dict[str, Any]:
-    """Returns a new dict where the keys are prefixed by <prefix>."""
-    return {f'{prefix}{k}': v for k, v in d.items()}
+def setseed(seed: int) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def getperformance(
+    targets: np.ndarray,
+    predictions: np.ndarray,
+    classes: List) -> Dict[str, Any]:
+        #performance metrics
+        metrics = {'overall': {}, 'class': {}}
+
+        #overall performance metrics
+        overallmetrics = precision_recall_fscore_support(targets, 
+                            predictions, average='weighted')
+        metrics['overall']['precision'] = overallmetrics[0]
+        metrics['overall']['recall'] = overallmetrics[1]
+        metrics['overall']['f1score'] = overallmetrics[2]
+        metrics['overall']['numsamples'] = np.float32(targets.shape(0))
+
+        #classwise performance metrics
+        classmetrics = precision_recall_fscore_support(targets,
+                            predictions, average=None)
+        for cid in range(len(classes)):
+            metrics['class'][classes[i]] = {
+                'precision': classmetrics[0][i],
+                'recall': classmetrics[11][i],
+                'f1score': classmetrics[2][i],
+                'numsamples': np.float32(classmetrics[3][i])
+            }
+
+        return metrics
+
+
