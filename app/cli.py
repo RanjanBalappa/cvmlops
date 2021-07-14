9c2b5357cda1dc42867dcec219bc6e80205fd5c8
Ubuntu <wrs@classifier-training.zkvmrbg1fsnurp0qjf35dbsoxc.jx.internal.cloudapp.net>
2021-07-14 05:58:38 +0000

    Implementation of typer cli api

--- app/cli.py
+++ app/cli.py
@@ -1,13 +1,69 @@
+from typing import Optional
 import typer
 from pathlib import Path
+import yaml
+import json
+from argparse import Namespace
+import tempfile
+
 import torch
-from cv import main, train
+import mlflow
+from cv import main, train, utils
 
 app = typer.Typer()
 
+BASEDIR = Path(__file__).parent.parent.absolute()
+CONFIGDIR = Path(BASEDIR, 'config')
+MODELDIR = Path(BASEDIR, 'model')
+
 
 @app.command()
-def hello():
-    print('Inside hello')
+def trainmodel(
+    paramsfile: Optional[Path] = Path(CONFIGDIR, 'config.json'),
+    experimentname: Optional[str] = 'best',
+    runname: Optional[str] = 'model',
+    modeldir: Optional[Path] = Path(MODELDIR)
+):
+    configfile = open(Path(paramsfile), 'r')
+    params = Namespace(**utils.load_dict(filepath=paramsfile))
 
+    #Start Run
+    mlflow.set_experiment(experiment_name=experimentname)
+    with mlflow.start_run(run_name=runname):
+        runid = mlflow.active_run().info.run_id
+
+        #Train the model
+        artifacts = main.trainmodel(params)
+        
+        #store artifacts
+        performance = artifacts['performance']
+        print(json.dumps(performance, indent=2))
+        metrics = {
+            'precision': performance['overall']['precision'],
+            'recall': performance['overall']['recall'],
+            'f1score': performance['overall']['f1score'],
+            'top1accuracy': performance['overall']['top1']
+
+        }
+
+        mlflow.log_metrics(metrics)
+
+        with tempfile.TemporaryDirectory() as dp:
+            utils.save_dict(performance, Path(dp, "performance.json"))
+            torch.save(artifacts["model"].state_dict(), Path(dp, "model.pt"))
+            mlflow.log_artifacts(dp)
+
+
+    # Save for repo
+    open(Path(modeldir, "run_id.txt"), "w").write(runid)
+    utils.save_dict(performance, Path(modeldir, "performance.json"))
+
+    
+
+
+
+    
+
+
+    
 
